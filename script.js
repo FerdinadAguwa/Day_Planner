c4e2a38999f2447702f1d351cb9992edfbf5ce7a
Ferdinad Aguwa <ferd.aguwa@yahoo.com>
2020-11-12 17:43:13 -0600

    created code for color blocks, next step figure out local storage

--- script.js
+++ script.js
@@ -12,7 +12,7 @@ var currentHour = parseInt(moment().format("h"));
 console.log(typeof(currentHour))
 
 
-
+// Color blocks based on the past(grey) presesnt(red) and future(green) events!
 $(`.row`).each(function(){
     var rowTime = parseInt($(this).attr("id"))
     console.log(rowTime)
@@ -25,6 +25,8 @@ $(`.row`).each(function(){
     }
     else{
         $(this).addClass('future');
-    }
-  
+    } 
 });
+
+
+
