5f0f8c7e71d6b660c6b7df7e494e53606daaf58e
Ferdinad Aguwa <ferd.aguwa@yahoo.com>
2020-11-13 23:19:54 -0600

    added some files to local storage

--- script.js
+++ script.js
@@ -4,8 +4,7 @@ var dateTime = moment();
 console.log(dateTime);
 var textLocationEl = $("#textLocation");
 var saveButton = $(".saveBtn");
-
-
+textLocationEl=""
 
 // Current day of the week
 $("#currentDay").append(` ${dateTime.format('dddd, MMMM Do')}`);
@@ -14,10 +13,25 @@ var currentHour = parseInt(moment().format("h"));
 console.log(typeof(currentHour))
 
 
+saveButton.click(function() {
+    var key = $(this).closest(".row").attr("id");
+    console.log(key);
+  
+    // get the text
+    var value =$("textarea").attr("id").value;
+    console.log(value)
+  
+    // save to localStorage
+    localStorage.setItem(key, value)
+    console.log(localStorage)
+  
+  })
+
+
 // Color blocks based on the past(grey) presesnt(red) and future(green) events!
 $(`.row`).each(function(){
     var rowTime = parseInt($(this).attr("id"))
-    console.log(rowTime)
+    // console.log(rowTime)
 
     if(rowTime < currentHour){
         $(this).addClass('past');
@@ -28,10 +42,6 @@ $(`.row`).each(function(){
     else{
         $(this).addClass('future');
     } 
+
 });
 
- function plannerText(){
-     
-  
-       
- }
